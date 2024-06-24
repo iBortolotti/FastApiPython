571b4d1f9a6d80d4d60b2212f3a8c6e76d9bc862
iBortolotti <arcanjo.bortolotti@gmail.com>
2024-06-24 11:42:24 -0300

    Atualização das dependencias e criação do requirements.txt seguindo link - https://web.dio.me/topics/configuracao-windows-11-desenvolvendo-sua-primeira-api-com-fastapi-python-e-docker?back=%2Ftrack%2Fcoding-future-vivo-python-ai-backend-developer&order=undefined&page=1&search=python%20e%20docker&tab=forum&track_id=70304c16-a7d8-4066-97de-16345e1653a6

--- workout_api/categoria/model.py
+++ workout_api/categoria/model.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from workout_api.contrib.models import BaseModel
 
 class CategoriaModel(BaseModel):
