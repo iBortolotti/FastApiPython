e99c6882118084c0c5d813038ba7a05360eb9f47
iBortolotti <arcanjo.bortolotti@gmail.com>
2024-06-24 09:19:31 -0300

    Inicialização do arquivo docker_compose.yml

--- workout_api/categoria/model.py
+++ workout_api/categoria/model.py
@@ -5,5 +5,5 @@ from workout_api.contrib.models import BaseModel
 class CategoriaModel(BaseModel):
     __tablename__ = 'Categoria'
     pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    nome: Mapped[str] = mapped_column(String(50), nullable=False)
+    nome: Mapped[str] = mapped_column(String(50),unique=True, nullable=False)
     atleta: Mapped['AtletaModel'] = relationship(back_populates='categoria')
--- workout_api/centro_treinamento/model.py
+++ workout_api/centro_treinamento/model.py
@@ -5,7 +5,7 @@ from workout_api.contrib.models import BaseModel
 class CentroTreinamentoModel(BaseModel):
     __tablename__ = 'Centros_Treinamento'
     pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    nome: Mapped[str] = mapped_column(String(50), nullable=False)
+    nome: Mapped[str] = mapped_column(String(50),unique=True, nullable=False)
     endereco: Mapped[str] = mapped_column(String(60), nullable=False)
     proprietario: Mapped[str] = mapped_column(String(30), nullable=False)
     atleta: Mapped['AtletaModel'] = relationship(back_populates='centro_treinamento')
